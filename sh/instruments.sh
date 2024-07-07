13cb857a16a0ee4d14dec1316ae039c6706b9f47
softwaredevelop <61334390+softwaredevelop@users.noreply.github.com>
2024-07-07 19:27:43 +0200

    feat: Add script to fetch trading instruments metadata

---
+++ sh/instruments.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+TRADING212_HOST=${TRADING212_HOST:-"demo.trading212.com"}
+
+curl -s -X GET \
+  https://"${TRADING212_HOST}"/api/v0/equity/metadata/instruments \
+  -H "Authorization: $TRADING212_API_KEY"
