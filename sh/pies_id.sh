a21b9d99dfad83f7a41b72527167a1de15d4b2ad
softwaredevelop <61334390+softwaredevelop@users.noreply.github.com>
2024-07-07 21:00:31 +0200

    feat: Add script to fetch trading pie

---
+++ sh/pies_id.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+TRADING212_HOST=${TRADING212_HOST:-"demo.trading212.com"}
+ID=${1:?"Error: ID is required as the first argument."}
+
+curl -s -X GET \
+  https://"${TRADING212_HOST}"/api/v0/equity/pies/"${ID}" \
+  -H "Authorization: $TRADING212_API_KEY" |
+  jq '.[]'
