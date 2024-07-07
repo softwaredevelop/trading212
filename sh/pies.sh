96a0d108847cc79066702b77dcfaf4da1a484ad5
softwaredevelop <61334390+softwaredevelop@users.noreply.github.com>
2024-07-07 20:31:41 +0200

    feat: Add script to fetch trading pies data

---
+++ sh/pies.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+TRADING212_HOST=${TRADING212_HOST:-"demo.trading212.com"}
+
+curl -s -X GET \
+  https://"${TRADING212_HOST}"/api/v0/equity/pies \
+  -H "Authorization: $TRADING212_API_KEY" |
+  jq '.[]'
