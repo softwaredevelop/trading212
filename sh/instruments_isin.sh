4a81e98db84c69220f0d9e75b6c5d166ecad5460
softwaredevelop <61334390+softwaredevelop@users.noreply.github.com>
2024-07-08 13:51:16 +0200

    feat: Update instruments_isin.sh to include type and currency parameters

--- sh/instruments_isin.sh
+++ sh/instruments_isin.sh
@@ -2,8 +2,10 @@
 
 TRADING212_HOST=${TRADING212_HOST:-"demo.trading212.com"}
 ISIN=${1:?"Error: TICKER is required as the first argument."}
+TYPE=${2:-"ETF"}
+CURRENCY=${3:-"EUR"}
 
 curl -s -X GET \
   https://"${TRADING212_HOST}"/api/v0/equity/metadata/instruments \
   -H "Authorization: $TRADING212_API_KEY" |
-  jq --arg isin "$ISIN" '.[] | select(.isin == $isin)'
+  jq --arg isin "$ISIN" --arg type "$TYPE" --arg currency "$CURRENCY" '.[] | select(.isin == $isin and .type == $type and .currencyCode == $currency)'
