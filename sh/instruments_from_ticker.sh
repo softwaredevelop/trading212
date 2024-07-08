a1a6f2a3a38b7de659b9d912ada98279ecd988f2
softwaredevelop <61334390+softwaredevelop@users.noreply.github.com>
2024-07-08 18:13:12 +0200

    feat: Add instruments_from_ticker.sh script to fetch trading instruments by ticker

---
+++ sh/instruments_from_ticker.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+TRADING212_HOST=${TRADING212_HOST:-"demo.trading212.com"}
+TICKER=${1:?"Error: TICKER is required as the first argument."}
+TYPE=${2:-"ETF"}
+CURRENCY=${3:-"EUR"}
+
+curl -s -X GET \
+  https://"${TRADING212_HOST}"/api/v0/equity/metadata/instruments \
+  -H "Authorization: $TRADING212_API_KEY" |
+  jq --arg ticker "$TICKER" --arg type "$TYPE" --arg currency "$CURRENCY" '.[] | select(.ticker == $ticker and .type == $type and .currencyCode == $currency)'
