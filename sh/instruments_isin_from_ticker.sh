6316654574cd0920e3ad56f2f7064c2f1264ffe6
softwaredevelop <61334390+softwaredevelop@users.noreply.github.com>
2024-07-08 15:24:21 +0200

    refactor: Rename instruments_ticker_isin.sh to instruments_isin_from_ticker.sh

--- sh/instruments_isin_from_ticker.sh
+++ sh/instruments_isin_from_ticker.sh
@@ -2,8 +2,10 @@
 
 TRADING212_HOST=${TRADING212_HOST:-"demo.trading212.com"}
 TICKER=${1:?"Error: TICKER is required as the first argument."}
+TYPE=${2:-"ETF"}
+CURRENCY=${3:-"EUR"}
 
 curl -s -X GET \
   https://"${TRADING212_HOST}"/api/v0/equity/metadata/instruments \
   -H "Authorization: $TRADING212_API_KEY" |
-  jq --arg ticker "$TICKER" -r '.[] | select(.ticker == $ticker) | .isin'
+  jq --arg ticker "$TICKER" --arg type "$TYPE" --arg currency "$CURRENCY" -r '.[] | select(.ticker == $ticker and .type == $type and .currencyCode == $currency) | .isin'
