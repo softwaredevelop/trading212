fced68eb8bce96d18491de4e57f5be538d059aaf
softwaredevelop <61334390+softwaredevelop@users.noreply.github.com>
2024-07-08 15:55:20 +0200

    refactor: Rename instruments_tickers_isins.sh to instruments_isins_from_tickers.sh

--- sh/instruments_isins_from_tickers.sh
+++ sh/instruments_isins_from_tickers.sh
@@ -2,11 +2,13 @@
 
 TRADING212_HOST=${TRADING212_HOST:-"demo.trading212.com"}
 TICKER_FILE=${1:?"Error: TICKER_FILE is required as the first argument."}
+TYPE=${2:-"ETF"}
+CURRENCY=${3:-"EUR"}
 
 INSTRUMENTS=$(curl -s -X GET \
   https://"${TRADING212_HOST}"/api/v0/equity/metadata/instruments \
   -H "Authorization: $TRADING212_API_KEY")
 
 while IFS= read -r TICKER; do
-  echo "$INSTRUMENTS" | jq --arg ticker "$TICKER" -r '.[] | select(.ticker == $ticker) | .isin'
+  echo "$INSTRUMENTS" | jq --arg ticker "$TICKER" --arg type "$TYPE" --arg currency "$CURRENCY" -r '.[] | select(.ticker == $ticker and .type == $type and .currencyCode == $currency) | .isin'
 done <"$TICKER_FILE"
