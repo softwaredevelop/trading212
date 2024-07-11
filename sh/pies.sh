f20055ea6bcbad8c4e563b262e823b096e4b8332
softwaredevelop <61334390+softwaredevelop@users.noreply.github.com>
2024-07-11 11:51:07 +0200

    refactor: Update Authorization header in pies.sh script

--- sh/pies.sh
+++ sh/pies.sh
@@ -4,5 +4,5 @@ TRADING212_HOST=${TRADING212_HOST:-"demo.trading212.com"}
 
 curl -s -X GET \
   https://"${TRADING212_HOST}"/api/v0/equity/pies \
-  -H "Authorization: $TRADING212_API_KEY" |
+  -H "Authorization: ${TRADING212_API_KEY}" |
   jq '.[]'
