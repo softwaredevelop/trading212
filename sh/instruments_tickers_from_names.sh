6bf989be5b48b29be9cf01767730ea94fd017423
softwaredevelop <61334390+softwaredevelop@users.noreply.github.com>
2024-07-11 09:49:10 +0200

    refactor: Update instruments_tickers_from_names.sh script to use variable interpolation for Authorization header

--- sh/instruments_tickers_from_names.sh
+++ sh/instruments_tickers_from_names.sh
@@ -7,8 +7,8 @@ CURRENCY=${3:-"EUR"}
 
 INSTRUMENTS=$(curl -s -X GET \
   https://"${TRADING212_HOST}"/api/v0/equity/metadata/instruments \
-  -H "Authorization: $TRADING212_API_KEY")
+  -H "Authorization: ${TRADING212_API_KEY}")
 
 while IFS= read -r NAME; do
-  echo "$INSTRUMENTS" | jq --arg name "$NAME" --arg type "$TYPE" --arg currency "$CURRENCY" -r '.[] | select(.name == $name and .type == $type and .currencyCode == $currency) | .ticker'
-done <"$NAME_FILE"
+  echo "${INSTRUMENTS}" | jq --arg name "${NAME}" --arg type "${TYPE}" --arg currency "${CURRENCY}" -r '.[] | select(.name == $name and .type == $type and .currencyCode == $currency) | .ticker'
+done <"${NAME_FILE}"
