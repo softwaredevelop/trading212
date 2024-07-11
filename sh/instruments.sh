5bfb216bc4b73f86f06f5737cbacd1b453eb9f1a
softwaredevelop <61334390+softwaredevelop@users.noreply.github.com>
2024-07-11 09:35:29 +0200

    refactor: Update Authorization header in instruments.sh script

--- sh/instruments.sh
+++ sh/instruments.sh
@@ -4,4 +4,4 @@ TRADING212_HOST=${TRADING212_HOST:-"demo.trading212.com"}
 
 curl -s -X GET \
   https://"${TRADING212_HOST}"/api/v0/equity/metadata/instruments \
-  -H "Authorization: $TRADING212_API_KEY"
+  -H "Authorization: ${TRADING212_API_KEY}"
