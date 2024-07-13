798bb6592e9c534f875dd1049e66e1a5de15b319
softwaredevelop <61334390+softwaredevelop@users.noreply.github.com>
2024-07-13 14:20:43 +0200

    refactor: Update pies_delete.sh script to use variable interpolation for Authorization header

--- sh/pies_delete.sh
+++ sh/pies_delete.sh
@@ -5,5 +5,5 @@ ID=${1:?"Error: ID is required as the first argument."}
 
 curl -s -X DELETE \
   https://"${TRADING212_HOST}"/api/v0/equity/pies/"${ID}" \
-  -H "Authorization: $TRADING212_API_KEY" |
+  -H "Authorization: ${TRADING212_API_KEY}" |
   jq '.[]'
