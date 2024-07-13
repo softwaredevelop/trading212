12be0bdbd5175f5b105b208d292fb77c80cd9539
softwaredevelop <61334390+softwaredevelop@users.noreply.github.com>
2024-07-13 16:09:10 +0200

    feat: Add script to retrieve instrument names from ISINs

---
+++ sh/instruments_names_from_isins.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+TRADING212_HOST=${TRADING212_HOST:-"demo.trading212.com"}
+ISIN_FILE=${1:?"Error: ISIN_FILE is required as the first argument."}
+TYPE=${2:-"ETF"}
+CURRENCY=${3:-"EUR"}
+
+INSTRUMENTS=$(curl -s -X GET \
+  https://"${TRADING212_HOST}"/api/v0/equity/metadata/instruments \
+  -H "Authorization: ${TRADING212_API_KEY}")
+
+while IFS= read -r ISIN; do
+  echo "${INSTRUMENTS}" | jq --arg isin "${ISIN}" --arg type "${TYPE}" --arg currency "${CURRENCY}" -r '.[] | select(.isin == $isin and .type == $type and .currencyCode == $currency) | .name'
+done <"${ISIN_FILE}"
